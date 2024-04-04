82f10d30bdb0ef7249bf68f6af362aa555f2d560
taldomelin <gumelo.cant@gmail.com>
2024-04-04 09:30:07 -0300

    Atualizando git

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { View } from "react-native";
 import VSBurgerCarrinho from "./src/VSBurgerCarrinho";
 import VSBurgerCardapio from "./src/VSBurgerCardapio";
+import CadastroProduto from "./src/screens/CadastroProduto";
 
 function App(): React.JSX.Element{
   return(
    
-      <VSBurgerCarrinho/>
+     //  <VSBurgerCarrinho/>
      // <VSBurgerCardapio/>
+     <CadastroProduto/>
+
    
   );
 }
